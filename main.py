a7144175dc40c2e9bae8c67a689d352f86745cb1
Soheil <itsupport@londonist.co.uk>
2024-08-14 18:04:50 +0300

    Refactor task input and list components
    Modularize the task input and task list into separate functions and simplify their imports. This change enhances code readability and maintainability by isolating specific functionalities.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fasthtml.common import *
 
-from components.task import new_task_input
+from components.task import new_task_input, task_list
 
 tailwind = Script(src="https://cdn.tailwindcss.com")
 app, rt = fast_app(hdrs=(tailwind,), debug=True, live=True, reload_interval=0.5)
@@ -11,12 +11,7 @@ tasks = ['task 1', 'task 2', 'task 3']
 @rt('/')
 def get():
     return (
-        Ul(
-            *[Li(task, cls='list-none') for task in tasks],
-            new_task_input,
-            cls='p-10 text-2xl ',
-            id='task_list',
-        )
+        task_list(tasks)
     )
 
 
@@ -25,7 +20,7 @@ def post(new_task: str):
     tasks.append(new_task)
     return (
         Li(new_task, cls='list-none'),
-        new_task_input
+        new_task_input()
     )
 
 
