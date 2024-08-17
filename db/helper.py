7d8326ef1201227b907202af8d4aa97735d13838
Mee1ad <soheilravasani@gmail.com>
2024-08-17 14:41:29 +0300

    Add get_space_by_id function and rename get_task_list_by_id
    Renamed the function get_task_list_with_id to get_task_list_by_id for clarity. Added a new function get_space_by_id to fetch space details, including associated task lists and tasks. This enhances database querying capabilities by providing more specific retrieval options.

--- db/helper.py
+++ db/helper.py
@@ -1,5 +1,25 @@
 from db.models.task import *
 
 
-def get_task_list_with_id(task_list_id: int):
-    return TaskList.select().join(TaskListTask).join(Task).where(TaskList.id == task_list_id).first()
+def get_task_list_by_id(task_list_id: int):
+    return (
+        TaskList
+        .select()
+        .join(TaskListTask)
+        .join(Task)
+        .where(TaskList.id == task_list_id)
+        .first()
+    )
+
+
+def get_space_by_id(space_id: int):
+    return (
+        Space
+        .select()
+        .join(SpaceTaskList)
+        .join(TaskList)
+        .join(TaskListTask)
+        .join(Task)
+        .where(Space.id == space_id)
+        .first()
+    )
