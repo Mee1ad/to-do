84570d08ca6d62e1d86090223ddc2b2320ae988d
Mee1ad <soheilravasani@gmail.com>
2024-08-17 09:14:51 +0300

    Refactor task management and update endpoints
    Changed import statements to reflect new module structure and improved the task management logic by introducing schema-based data handling. Added a new endpoint to handle task checkbox state updates and refined existing ones for listing and adding tasks.

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
-from fasthtml.common import *
-
-from components.task import new_task_input, task_list
+from components.task_components import *
+from schemas.task import *
 
 tailwind = Script(src="https://cdn.tailwindcss.com")
-app, rt = fast_app(hdrs=(tailwind,), debug=True, live=True, reload_interval=0.5)
-
-tasks = ['task 1', 'task 2', 'task 3']
+app, rt = fast_app(hdrs=(tailwind,), debug=True, live=True, reload_interval=1)
 
 
 @rt('/')
 def get():
+    task_list: TaskListSchema = TaskList.select().join(TaskListTask).join(Task).where(TaskList.id == 1).first()
     return (
-        task_list(tasks)
+        task_list_view(task_list)
     )
 
 
 @rt('/add_task')
 def post(new_task: str):
-    tasks.append(new_task)
+    task = Task.create(title=new_task)
     return (
-        Li(new_task, cls='list-none'),
-        new_task_input()
+        task_item(task),
+        new_task_input_field()
     )
+
+
+@rt('/task_checkbox')
+def put(task_id: int):
+    query = Task.update(checked=(Task.checked == 0)).where(Task.id == task_id)
+    query.execute()
+    task = Task.get(id=task_id)
+    return task_checkbox(task)
 
 
 serve()
