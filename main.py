a81b6c8e1d8f69a878c46f6f32d7d0a95b7e0bd5
Mee1ad <soheilravasani@gmail.com>
2024-08-17 14:43:26 +0300

    Add new endpoints for task list creation and updating
    Introduced `/task_list` POST endpoint to create new task lists and `/task_list` PUT endpoint to update existing task lists. Updated the main endpoint to fetch and render a space view instead of a task list view.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pydantic import ValidationError
 
 from components.task_components import *
 from schemas.task import *
+from db.helper import *
 
 tailwind = Script(src="https://cdn.tailwindcss.com")
 app, rt = fast_app(hdrs=(tailwind,), debug=True, live=True, reload_interval=1)
@@ -9,9 +10,14 @@ app, rt = fast_app(hdrs=(tailwind,), debug=True, live=True, reload_interval=1)
 
 @rt('/')
 def get():
-    task_list: TaskListSchema = TaskList.select().join(TaskListTask).join(Task).where(TaskList.id == 1).first()
+    space = get_space_by_id(1)
     return (
-        task_list_view(task_list)
+        Div(
+            space_view(space),
+
+            cls='pt-10 pl-10',
+        )
+
     )
 
 
@@ -27,6 +33,31 @@ def post(task_text: str, tasklist_id: int):
         task_item(task),
         new_task_input_field(tasklist_id)
     )
+
+
+@rt('/task_list')
+def post(task_list_title: str, space_id: int):
+    try:
+        TaskListCreateSchema(task_list_title=task_list_title, space_id=space_id)
+    except ValidationError as e:
+        return JSONResponse({"errors": e.errors()}, status_code=400)
+    task_list = TaskList.create(title=task_list_title.capitalize())
+    space_task_list = SpaceTaskList.create(space_id=space_id, task_list_id=task_list.id)
+    return (
+        task_list_view(task_list),
+        new_task_list_title_view(task_list.id),
+    )
+
+
+@rt('/task_list')
+def put(task_list_id: int, task_list_title: str):
+    try:
+        TaskListUpdateSchema(id=task_list_id, title=task_list_title)
+    except ValidationError as e:
+        return JSONResponse({"errors": e.errors()}, status_code=400)
+    TaskList.update(title=task_list_title.capitalize()).where(TaskList.id == task_list_id).execute()
+    task_list = TaskList.get(TaskList.id == task_list_id)
+    return task_list_title_view(task_list)
 
 
 @rt('/task_checkbox')
