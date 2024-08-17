df4260b813bea74fac4b1da79a07e51a514d0303
Mee1ad <soheilravasani@gmail.com>
2024-08-17 16:57:40 +0300

    Refactor endpoints and enhance space management
    Refactor existing routes to improve readability and functionality. Introduce new route for creating spaces with improved UI feedback. Additionally, adjust the existing task checkbox update logic for consistency.

--- main.py
+++ main.py
@@ -10,18 +10,22 @@ app, rt = fast_app(hdrs=(tailwind,), debug=True, live=True, reload_interval=1)
 
 @rt('/')
 def get():
+    spaces = Space.select()
     space = get_space_by_id(1)
-    return (
-        Div(
-            space_view(space),
-
-            cls='pt-10 pl-10',
-        )
-
+    return Div(
+        spaces_list_view(spaces),
+        space_view(space),
+        cls='flex gap-8 pt-10 pl-10',
     )
 
 
-@rt('/add_task')
+@rt('/space/{space_id}/{space_title}')
+def get(space_id: int, space_title: str):
+    space = get_space_by_id(space_id)
+    return space_view(space)
+
+
+@rt('/task')
 def post(task_text: str, tasklist_id: int):
     try:
         TaskCreateSchema(task_text=task_text, tasklist_id=tasklist_id)
@@ -33,6 +37,14 @@ def post(task_text: str, tasklist_id: int):
         task_item(task),
         new_task_input_field(tasklist_id)
     )
+
+
+@rt('/task_checkbox')
+def put(task_id: int):
+    query = Task.update(checked=(Task.checked == 0)).where(Task.id == task_id)
+    query.execute()
+    task = Task.get(id=task_id)
+    return task_checkbox(task)
 
 
 @rt('/task_list')
@@ -60,12 +72,37 @@ def put(task_list_id: int, task_list_title: str):
     return task_list_title_view(task_list)
 
 
-@rt('/task_checkbox')
-def put(task_id: int):
-    query = Task.update(checked=(Task.checked == 0)).where(Task.id == task_id)
-    query.execute()
-    task = Task.get(id=task_id)
-    return task_checkbox(task)
+@rt('/space')
+def post(space_title: str):
+    try:
+        SpaceCreateSchema(title=space_title)
+    except ValidationError as e:
+        return JSONResponse({"errors": e.errors()}, status_code=400)
+    space = Space.create(title=space_title.capitalize())
+    return (
+        P(space.title,
+          hx_get=f'/space/{space.id}/{space.title}',
+          hx_trigger=f'click',
+          hx_target=f'#space_view',
+          hx_swap='outerHTML transition:true',
+          hx_transition_in='fade-in-scale-up',
+          cls='p-2'
+          ),
+        Input(
+            type='text',
+            name='task_space',
+            id='new_space',
+            placeholder='Add Space',
+            autocomplete='off',
+            hx_post='/space',
+            hx_trigger=f'keyup[{ENTER_KEY_CODE}]',
+            hx_target=f'#space_list',
+            hx_swap='beforeend transition:true',
+            hx_transition_in='fade-in-scale-up',
+            cls='border-none focus:border-none !p-2'
+        )
+
+    )
 
 
 serve()
