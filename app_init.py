2c9398b4d91b0ba4d99890e2394fb58284ddf68e
Soheil <itsupport@londonist.co.uk>
2024-09-04 12:22:32 +0300

    replace dragula with sortable

--- app_init.py
+++ app_init.py
@@ -5,13 +5,12 @@ tailwind = Script(src="https://cdn.tailwindcss.com")
 tailwind_forms = Script(src="https://cdn.jsdelivr.net/npm/@tailwindcss/forms@0.5.7/src/index.min.js")
 font_awesome = Script(src='https://kit.fontawesome.com/0371c877a9.js', crossorigin='anonymous')
 tailwind_settings = Script(src="tailwind.config.js")
-dragula = Script(src="https://cdnjs.cloudflare.com/ajax/libs/dragula/3.7.3/dragula.min.js")
-dragula_css = Link(rel="stylesheet", href="https://cdnjs.cloudflare.com/ajax/libs/dragula/3.7.3/dragula.min.css")
+sortable = Script(src="https://cdn.jsdelivr.net/npm/sortablejs@latest/Sortable.min.js")
 global_css = Link(rel="stylesheet", href="global.css", type="text/css")
 # tailwind_animate = Script(src='https://cdn.jsdelivr.net/npm/tailwindcss-animated@1.1.2/src/index.min.js')
 font_inter = Link(rel="stylesheet", href="https://rsms.me/inter/inter.css")
 css = Style('body { font-family: Inter var, sans;}')
 
 app, rt = fast_app(
-    hdrs=(tailwind, tailwind_settings, tailwind_forms, global_css, font_inter, css, font_awesome, dragula, dragula_css),
+    hdrs=(tailwind, tailwind_settings, tailwind_forms, global_css, font_inter, css, font_awesome, sortable),
     debug=True, live=True, reload_interval=1, pico=False)
