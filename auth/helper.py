70e3300ab83c00ce29e228f57c2fb6c1452848e7
Mee1ad <soheilravasani@gmail.com>
2024-09-08 19:52:50 +0300

    update login redirect

--- auth/helper.py
+++ auth/helper.py
@@ -12,7 +12,7 @@ workos_client = WorkOSClient(api_key=env.workos_api_key, client_id=env.workos_cl
 
 def get_login_url() -> str:
     provider = "GoogleOAuth"
-    redirect_uri = f"{env.domain}/auth/callback"
+    redirect_uri = f"https://{env.domain}/auth/callback"
     authorization_url = workos_client.sso.get_authorization_url(
         provider="GoogleOAuth", redirect_uri=redirect_uri
     )
