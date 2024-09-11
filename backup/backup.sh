966600800b18039980eb523d3c7f38bb7b3b2b42
Soheil <itsupport@londonist.co.uk>
2024-09-11 10:26:57 +0300

    update db backup script to use db container

--- backup/backup.sh
+++ backup/backup.sh
@@ -3,11 +3,12 @@
 TIMESTAMP=$(date +%F_%T)
 BACKUP_FILE="/backup/db_backup_${TIMESTAMP}.sql"
 
-pg_dump -U $POSTGRES_USER -d $POSTGRES_DB -F c > $BACKUP_FILE
+pg_dump -h db -U "$POSTGRES_USER" -d "$POSTGRES_DB" -F c > "$BACKUP_FILE"
 
 if [ $? -eq 0 ]; then
   echo "Backup successful: $BACKUP_FILE"
 else
   echo "Backup failed"
+  rm -f "$BACKUP_FILE"
   exit 1
 fi
