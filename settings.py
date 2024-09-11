534f1d0a973ba634840689e3e4e47aad80c8f312
Soheil <itsupport@londonist.co.uk>
2024-09-11 09:44:21 +0300

    add postgres connection to project

--- settings.py
+++ settings.py
@@ -1,18 +1,15 @@
-from peewee import SqliteDatabase, Model
+from peewee import SqliteDatabase, PostgresqlDatabase, Model
 from pydantic_settings import BaseSettings
-
-DB = SqliteDatabase('./db/to-do.sqlite3', pragmas={'foreign_keys': 1})
-
-
-class BaseModel(Model):
-    class Meta:
-        database = DB
 
 
 class Env(BaseSettings):
+    stage: str
     domain: str
     workos_api_key: str
     workos_client_id: str
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str
 
     class Config:
         env_file = '.env'
@@ -20,3 +17,19 @@ class Env(BaseSettings):
 
 
 env = Env()
+
+if env.stage.lower() == 'local':
+    DB = SqliteDatabase('./db/to-do.sqlite3', pragmas={'foreign_keys': 1})
+elif env.stage.lower() == 'prod':
+    db = PostgresqlDatabase(
+        env.postgres_db,
+        user=env.postgres_user,
+        password=env.postgres_password,
+        host='db',
+        port=5432
+    )
+
+
+class BaseModel(Model):
+    class Meta:
+        database = DB
