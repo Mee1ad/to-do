036ffdb931204b834195660043525b90a3d92cea
Soheil <itsupport@londonist.co.uk>
2024-09-13 17:35:40 +0300

    test session 2

--- main.py
+++ main.py
@@ -15,17 +15,15 @@ stage = env.stage.lower()
 
 
 @app.get('/')
-async def home_page(scope: Dict[str, Any], session):
-    print(scope)
-    if stage == 'prod':
-        session = scope.get('session', {})
+async def home_page(session):
+    print(session)
     user: UserSchema = get_user_from_session(session)
     if not user:
         user: UserSchema = User.create(name='Guest')
         Login.create(user_id=user.id, provider='session', connection_id=uuid.uuid4().hex)
         session['user_id'] = user.id
-        if stage == 'prod':
-            REDIS.hmset(session.id, session)
+        # if stage == 'prod':
+        #     REDIS.hmset(session['session_id']['user_id'], session)
     spaces = Space.select().where(Space.user_id == user.id).order_by(Space.order).execute()
     first_space = Space.select().where(Space.user_id == user.id).order_by(Space.order).first()
     first_space_id = first_space.id if first_space else None
