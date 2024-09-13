68a8d18ea3af9deaaa29dae97dd699d0a3da2091
Soheil <itsupport@londonist.co.uk>
2024-09-13 17:37:53 +0300

    test session 4

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ async def home_page(session):
         Login.create(user_id=user.id, provider='session', connection_id=uuid.uuid4().hex)
         session['user_id'] = user.id
         if stage == 'prod':
-            REDIS.hmset(session['session_id']['user_id'], session)
+            REDIS.hmset(session['user_id'], session)
     spaces = Space.select().where(Space.user_id == user.id).order_by(Space.order).execute()
     first_space = Space.select().where(Space.user_id == user.id).order_by(Space.order).first()
     first_space_id = first_space.id if first_space else None
