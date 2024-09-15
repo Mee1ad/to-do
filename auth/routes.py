ce4a2588368e4607973d8f10db42de62f148ebb0
Mee1ad <soheilravasani@gmail.com>
2024-09-15 21:19:23 +0300

    bugfix

--- auth/routes.py
+++ auth/routes.py
@@ -10,18 +10,22 @@ def callback(req, code: str):
     profile_and_token = workos_client.sso.get_profile_and_token(code)
     profile = profile_and_token.profile
     user = User.select().where(User.name == profile.first_name, User.email == profile.email).first()
+    print('user is', user.name)
     if not user:
+        print('user not found')
         redis_session = req.scope['redis_session']
         guest_user_id = redis_session.get('user_id')
         user = User.select().where(User.name == 'Guest', User.id == guest_user_id).first()
+        print('user is', user)
         user.name = profile.first_name
         user.email = profile.email
         user.save()
+
     Login.get_or_create(user=user.id, provider=profile.connection_type,
                         connection_id=profile.connection_id, idp_id=profile.idp_id,
                         defaults={'user': user.id, 'provider': profile.connection_type,
                                   'connection_id': profile.connection_id, 'idp_id': profile.idp_id})
-
+    req.scope['redis_session']['user_id'] = user.id
     return RedirectResponse("/")
 
 
