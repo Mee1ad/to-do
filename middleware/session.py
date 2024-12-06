226b8c318687f2b3667cdaf8d5b486f726845419
Mee1ad <soheilravasani@gmail.com>
2024-12-06 15:41:14 +0300

    session fix 1

--- middleware/session.py
+++ middleware/session.py
@@ -46,6 +46,7 @@ class RedisSessionMiddleware(MiddlewareBase):
                     redis_session_byte_dict = await self.redis_client.hgetall(session_id)
                 except (DataError, AuthenticationError) as e:
                     logging.error(f"Redis DataError: {str(e)}")
+                    return
 
             scope['redis_session'] = {
                 key.decode('utf-8'): value.decode('utf-8')
